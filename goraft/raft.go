722374bf012c215a24a3f6abe6ffa5a47b0593c6
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-05-17 12:54:23 +0330

    feat: persist func for storing states

--- goraft/raft.go
+++ goraft/raft.go
@@ -1,6 +1,8 @@
 package goraft
 
 import (
+	"bufio"
+	"encoding/binary"
 	"fmt"
 	"net/http"
 	"net/rpc"
@@ -176,4 +178,99 @@ func Assert[T comparable](msg string, a, b T) {
 
 func Server_assert[T comparable](s *Server, msg string, a, b T) {
 	Assert(s.debugmsg(msg), a, b)
+}
+
+// currentTerm, log, and votedFor must be persisted to disk as they're edited
+
+// func (s *Server) persist() {
+// 	s.mu.Lock()
+// 	defer s.mu.Unlock()
+
+// 	s.fd.Truncate(0)
+// 	s.fd.Seek(0, 0)
+
+// 	enc := gob.NewEncoder(s.fd)
+// 	err := enc.Encode(PersistentState{
+// 		CurrentTerm: s.currentTerm,
+// 		Log:         s.log,
+// 		VotedFor:    s.votedFor,
+// 	})
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	if err = s.fd.Sync(); err != nil {
+// 		panic(err)
+// 	}
+
+// 	s.debug(fmt.Sprintf("Persisted. Term: %d. Log Len: %d. Voted For: %s.", s.currentTerm, len(s.log), s.votedFor))
+// }
+
+const PAGE_SIZE = 4096
+const ENTRY_HEADER = 16
+const ENTRY_SIZE = 128
+
+// Must be called within s.mu.Lock()
+func (s *Server) persist(writeLog bool, nNewEntries int) {
+	t := time.Now()
+
+	if nNewEntries == 0 && writeLog {
+		nNewEntries = len(s.log)
+	}
+
+	s.fd.Seek(0, 0)
+
+	var page [PAGE_SIZE]byte
+	// Bytes 0  - 8:   Current term
+	// Bytes 8  - 16:  Voted for
+	// Bytes 16 - 24:  Log length
+	// Bytes 4096 - N: Log
+
+	binary.LittleEndian.PutUint64(page[:8], s.currentTerm)
+	binary.LittleEndian.PutUint64(page[8:16], s.getVotedFor())
+	binary.LittleEndian.PutUint64(page[16:24], uint64(len(s.log)))
+	n, err := s.fd.Write(page[:])
+	if err != nil {
+		panic(err)
+	}
+	Server_assert(s, "Wrote full page", n, PAGE_SIZE)
+
+	if writeLog && nNewEntries > 0 {
+		newLogOffset := max(len(s.log)-nNewEntries, 0)
+
+		s.fd.Seek(int64(PAGE_SIZE+ENTRY_SIZE*newLogOffset), 0)
+		bw := bufio.NewWriter(s.fd)
+
+		var entryBytes [ENTRY_SIZE]byte
+		for i := newLogOffset; i < len(s.log); i++ {
+			// Bytes 0 - 8:    Entry term
+			// Bytes 8 - 16:   Entry command length
+			// Bytes 16 - ENTRY_SIZE: Entry command
+
+			if len(s.log[i].Command) > ENTRY_SIZE-ENTRY_HEADER {
+				panic(fmt.Sprintf("Command is too large (%d). Must be at most %d bytes.", len(s.log[i].Command), ENTRY_SIZE-ENTRY_HEADER))
+			}
+
+			binary.LittleEndian.PutUint64(entryBytes[:8], s.log[i].Term)
+			binary.LittleEndian.PutUint64(entryBytes[8:16], uint64(len(s.log[i].Command)))
+			copy(entryBytes[16:], []byte(s.log[i].Command))
+
+			n, err := bw.Write(entryBytes[:])
+			if err != nil {
+				panic(err)
+			}
+			Server_assert(s, "Wrote full page", n, ENTRY_SIZE)
+		}
+
+		err = bw.Flush()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if err = s.fd.Sync(); err != nil {
+		panic(err)
+	}
+	s.debugf("Persisted in %s. Term: %d. Log Len: %d (%d new). Voted For: %d.", time.Now().Sub(t), s.currentTerm, len(s.log), nNewEntries, s.getVotedFor())
 }
